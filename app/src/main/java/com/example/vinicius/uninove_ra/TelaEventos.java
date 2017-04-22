4cd1975b24db10f84c9722769b51cd6a407e118e
Delayzinho000 <viniciusalmeida14@yahoo.com>
2017-04-22 14:16:40 -0300

    Classe / Tela de eventos criada !

---
+++ app/src/main/java/com/example/vinicius/uninove_ra/TelaEventos.java
@@ -0,0 +1,13 @@
+package com.example.vinicius.uninove_ra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaEventos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_eventos);
+    }
+}
